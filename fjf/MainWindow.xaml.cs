14f3ca97d3de6098d5d11df0732b7669c2dfe289
dasha170718 <127710289+dasha170718@users.noreply.github.com>
2023-05-12 20:36:18 +0700

    Добавьте файлы проекта.

---
+++ fjf/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace fjf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (Tex1.SelectedItem != null)
+            {
+                Tex1.Items.Remove(Tex1.SelectedItem);
+            }
+            if (List1.SelectedItem != null)
+            {
+                List1.Items.Remove(List1.SelectedItem);
+            }
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (nadpis.Text != null)
+            {
+                Tex1.Items.Add(nadpis.Text);
+            }
+        }
+
+        private void Tex1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            if (Tex1.SelectedItem != null)
+            {
+                List1.Items.Add(Tex1.SelectedItem);
+                Tex1.Items.Remove(Tex1.SelectedItem);
+            }
+        }
+    }
+}
